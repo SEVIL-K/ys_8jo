1809864ee67d8c8b035075ebff131013d7ab5c30
GSC <141592320+tkdcks7@users.noreply.github.com>
2023-10-10 10:52:05 +0900

    에러체크용 commit

--- articles/models.py
+++ articles/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-# from accounts.models import User
+from accounts.models import User
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="loginuser")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="article")
 
-    # likes = models.ManyToManyField(User, related_name="likes", blank=True)
+    likes = models.ManyToManyField(User, related_name="likes", blank=True)
 
     def __str__(self):
         return str(self.title)
@@ -18,7 +18,7 @@ class Comment(models.Model):
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    # author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="loginuser")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comment")
     review = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="comment_set")
 
 
